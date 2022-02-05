4b233739024fc0e7cdde4f6bdb3a8584bb4328cc
Jeferson Mesquita <je_2742@hotmail.com>
2022-02-04 23:04:24 -0300

    Boilerplate Done
    Boilerplate Done

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-vars-requires
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+  },
+}),
+
 module.exports = {
   reactStrictMode: true,
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Aprenda NextJs, GraphQL/GraphCMS e Leaflet na Prática</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJs and Styled Components"
